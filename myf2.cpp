5cb14482121625bd6bbd29274069234c36cca9a8
3MEEHOCETC <tareycoylor@gmail.com>
2026-03-29 20:59:03 +0500

    cleaning

--- myf2.cpp
+++
@@ -1,9 +0,0 @@
-#include <cstdio>
-int main(){
-    int money = 100;
-
-    printf("Value: %d\n", money);
-    printf("Address: %p\n", (void*)&money);//void* универсальная записка с адресом -
-    // ему все равно что там, просто адрес в памяти
-    return 0;
-}
