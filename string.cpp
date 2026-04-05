4bd3c41995c17a5d44c75b9984c25b269a37d7fb
3MEEHOCETC <tareycoylor@gmail.com>
2026-04-05 19:55:45 +0500

    day 16

--- string.cpp
+++ string.cpp
@@ -8,4 +8,9 @@ int main()
 std::string word2("Over");
 std::string word3(3, '!');
 std::string phrase = word1 + " " + word2 + word3;
-std::cout << "The phrase is: << phrase << "\n\n";
+std::cout << "The phrase is: " << phrase << "\n\n";
+std::cout << "The phrase has " << phrase.size() << " characters in it.\n\n";
+std::cout << "the character at position 0 is: " << phrase[0] << "\n\n";
+std::cout << "Changing the character at position 0.\n";
+phrase[0] = 'L';
+std::cout << "the phrase is now: " << phrase << "\n\n"; 
