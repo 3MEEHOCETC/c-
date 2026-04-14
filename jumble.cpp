6213e06c214f146518f70ca42d95eb3564fc76fe
3MEEHOCETC <tareycoylor@gmail.com>
2026-04-14 13:39:34 +0500

    14 april

---
+++ jumble.cpp
@@ -0,0 +1,14 @@
+// Игра Word Jumble
+// Игра-головоломка с разгадыванием слов
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+
+int main()
+{
+    enum fields {WORD. HINT. NUM_FIELDS};
+    const int NUM_WORDS = 5;
+    const string WORDS[NUM_WORDS][NUM_FIELDS] = 
+    {
+        
