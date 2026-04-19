9a57d7704209a7a93d61db1307ec9d962cef6dcc
3MEEHOCETC <tareycoylor@gmail.com>
2026-04-19 18:14:40 +0500

    19 april

--- cengage.cpp
+++ cengage.cpp
@@ -1,43 +1,45 @@
-// Программа Hero's Invertory
-// Демонстрирует работу с массивами
+// Программа Hero's Inventory 2.0
+// Демонстрирует работу с векторами
 #include <iostream>
 #include <string>
-int main()
-{
-    const int MAX_ITEMS = 10;
-    std::string invertory[MAX_ITEMS];
-    int numItems = 0;
-    invertory[numItems++] = "sword";
-    invertory[numItems++] = "armor";
-    invertory[numItems++] = "shield";
-    std::cout << "Your items:\n";
-    for (int i = 0; i < numItems; ++i)
+#include <vector>
+
+int main() {
+    std::vector<std::string> invertory;
+    invertory.push_back("sword");
+    invertory.push_back("armor");
+    invertory.push_back("shield");
+    std::cout << "You have " << invertory.size() << " items.\n";
+    std::cout << "\nYour items:\n";
+    for (unsigned int i = 0; i < invertory.size(); ++i)
     {
     std::cout << invertory[i] << std::endl;
-}
+    }
     std::cout << "\nYou trade your sword for a battle axe.";
     invertory[0] = "battle axe";
     std::cout << "\nYour items:\n";
-    for (int i = 0; i < numItems; ++i)
-{
-    std::cout << invertory[i] << std::endl;
-}
-    std::cout << "\nThe item name '" << invertory[0] << "' has ";
-    std::cout << invertory[0].size() << " letters in it.\n";
-    std::cout << "\nYou find a healing potion.";
-    if (numItems < MAX_ITEMS)
+    for (unsigned int i = 0; i < invertory.size(); ++i)
     {
-    invertory[numItems++] = "healing potion";
+std::cout << invertory[i] <<std::endl;
 }
-else 
-{
-    std::cout << "You have too many items and can't carry another.";
-}
+std::cout << "\nThe item name '" << invertory[0] << "' has ";
+std::cout << invertory[0].size() << " letters in it.\n";
+std::cout << "\nYour shield is destroyed in a fierce battle.";
+invertory.pop_back();
 std::cout << "\nYour items:\n";
-for (int i = 0; i < numItems; ++i)
+for (unsigned int i = 0; i < invertory.size(); ++i)
 {
-    std::cout << invertory[i] << std::endl;
+std::cout << invertory[i] << std::endl;
+}
+std::cout << "\nYou were robbed of all of your possessions by a thief.";
+invertory.clear();
+if (invertory.empty())
+{
+std::cout << "\nYou have nothing.\n";
+}
+else
+{
+std::cout << "\nYou have at least one item.\n";
 }
 return 0;
 }
-
