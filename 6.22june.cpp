0b22b4f431666e57566d3640822163ead646c5d4
3MEEHOCETC <tareycoylor@gmail.com>
2026-06-06 20:29:32 +0500

    6.23 june

---
+++ 6.22june.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int return5()
+{
+    return 5;
+}
+
+int return8()
+{
+    return 8;
+}
+int main() {
+    return5();
+    return8();
+
+    return 0;   
+}
---
+++ 6.23june.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+void printO()
+{
+    std::cout << "O" << std::endl;
+}
+
+int main()
+{
+    std::cout << printO() << std::endl;
+
+    return 0;
+}
