4d6dc59e530d2012184b0d025f1f2e2e1c223485
3MEEHOCETC <tareycoylor@gmail.com>
2026-06-06 19:58:21 +0500

    6 june

---
+++ 6june.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+void printO()
+{
+    std::cout << "O" << std::endl;
+}
+
+void printK()
+{
+    std::cout << "K" << std::endl;
+}
+
+void printOK()
+{
+    printO();
+    printK();
+}
+
+//объявление main()
+int main()
+{
+    std::cout << "Starting main()" << std::endl;
+printOK();
+std::cout << "Ending main()" << std::endl;
+return 0;
+}
