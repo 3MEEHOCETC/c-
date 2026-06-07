52d74ad143bbf2d992e76fffb6eef2669e306fc8
3MEEHOCETC <tareycoylor@gmail.com>
2026-06-07 21:09:26 +0500

    7.6 june

---
+++ 7.6june.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int add(int a, int b, int c)
+{
+    return a + b + c;
+}
+
+int multiply(int a, int b)
+{
+    return a * b;
+}
+
+int main()
+{
+    std::cout << multiply(add(3, 4, 5), 5) << std::endl;
+    return 0;
+}
