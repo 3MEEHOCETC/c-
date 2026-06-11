9493f840ddfa5b29d4e0eb70b218ab9943ebf11b
3MEEHOCETC <tareycoylor@gmail.com>
2026-06-11 21:47:02 +0500

    11.2 june

--- 11june.cpp
+++ 11june.cpp
@@ -9,8 +9,8 @@ int main()
     int y;
     std::cin >> y;  
     std::cout << "x + y = " << (x + y) << std::endl;
-    //std::cout << x << " + " << y << " is " << x + y << "\.n"; 
+    //std::cout << x << " + " << y << " is " << x + y << ".\n"; 
     std::cout << "x - y = " << (x - y) << std::endl;
-    //std::cout << x << " - " << y << " is " << x - y << "\.n";
+    //std::cout << x << " - " << y << " is " << x - y << ".\n";
     return 0;
 }
