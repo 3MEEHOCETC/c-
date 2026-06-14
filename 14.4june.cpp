6d2c949518fa3de0bdec5fca800daafb64d810fa
3MEEHOCETC <tareycoylor@gmail.com>
2026-06-14 22:32:51 +0500

    14.4june.cpp

---
+++ 14.4june.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int return 5() // при объявлении функции нельзя ставить пробелы
+{
+    return 5;
+}
+
+int main()
+{
+    std::cout << return 5() << '\n';
+
+    return 0;
+}
