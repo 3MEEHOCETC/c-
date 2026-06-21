ebd14b05185c652d3b72a9201db5d00ac5f869e4
3MEEHOCETC <tareycoylor@gmail.com>
2026-06-21 11:59:26 +0500

    21.2june

---
+++ 21.2june.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Enter an integer: ";
+    int x{}; // x defined here
+    std::cin >> x; // and used here
+
+    std::cout << "Enter another integer: ";
+    int y{}; // y defined here
+    std::cin >> y; // and used here
+
+    int sum{x + y}; // sum can be initialized with intended value
+    std::cout << "The sum is: " << sum << '\n';
+
+    return 0;
+}
