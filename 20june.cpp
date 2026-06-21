3862227d87126f45f002b14fc1e92266e1b1fdb4
3MEEHOCETC <tareycoylor@gmail.com>
2026-06-21 11:15:56 +0500

    20th june

--- 20june.cpp
+++ 20june.cpp
@@ -2,5 +2,14 @@
 
 void doSomething()
 {
-    std::cout << "Hello!"\n";
+    std::cout << "Hello!\n";
 }
+
+int main()
+{
+    int x{0}; // x's lifetime begins here
+
+    doSomething(); // x is still alive during this function call 
+
+    return 0;
+} // x's lifetime ends here
