21a3b167511a5ff0245052021e4f93d7db7427cf
3MEEHOCETC <tareycoylor@gmail.com>
2026-06-25 12:37:36 +0500

    25.3 june

---
+++ 25.3june.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    #ifndef PRINT_JOSH     // this program prints "Josh" because PRINT_JOSH was never #defined - #infdef is the opposite of #ifdef
+    std::cout << "Josh\n";
+    #endif
+
+    return 0;
+}
