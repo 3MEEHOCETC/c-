fc76000e6129f6733e54b90517f4199eea9757ef
3MEEHOCETC <tareycoylor@gmail.com>
2026-06-26 15:27:13 +0500

    26.2 june

---
+++ 26.2june.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "function.cpp"
+void doSomething(); // forward declaration for function doSomething()
+
+#define PRINT
+
+int main()
+{
+    doSomething();
+
+    return 0;
+}
