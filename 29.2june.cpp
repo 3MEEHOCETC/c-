050af2b515e52df64f870b48834465fcc1d02616
3MEEHOCETC <tareycoylor@gmail.com>
2026-06-30 11:12:00 +0500

    29.2june

--- 29.2june.cpp
+++ 29.2june.cpp
@@ -8,27 +8,30 @@
 // readNumber - should be used to get (and return) a single integer gtom the user
 // writeAnswer - should be used to output the answer - it takes single parameter with no return value (void?)
 // main() - used to glue the above functions together
+void writeAnswer();
 
 int readNumber()
 {
     std::cout << "Enter an integer: " << '\n';
-    int x;
-    std::cin >> x;
-    return x;
-
-
+    int input;
+    std::cin >> input;
+    return input;
 }
 
 void writeAnswwer()
 {
-    std::cout << " x " << " + " << "y " << " x + y = " << '\n';
+    int x { readNumber() };
+    int y { readNumber() };
+    
+
+    std::cout << x << " x + y " << y  <<   x + y  << '\n';
 }
 
 int main()
 {
-    int x;
-    int readNumber(x);
-    void writeAnswer();
+    int x {readNumber()};
+    int y {readNumber()};
+    writeAnswer();
 
     return 0;
 }
