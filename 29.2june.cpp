432b18d0e7a48213eeb5098f5a858e0e34d43004
3MEEHOCETC <tareycoylor@gmail.com>
2026-07-02 11:11:31 +0500

    2th july

--- 29.2june.cpp
+++ 29.2june.cpp
@@ -1,4 +1,6 @@
-#include <iostream>
+
+#include "io.h"
+
 
 // reads 2 separate intgers from user
 // adds them together 
@@ -8,10 +10,6 @@
 // readNumber - should be used to get (and return) a single integer gtom the user
 // writeAnswer - should be used to output the answer - it takes single parameter with no return value (void?)
 // main() - used to glue the above functions together
-
-int readNumber();
-
-void writeAnswer(int x);
 
 int main()
 {
