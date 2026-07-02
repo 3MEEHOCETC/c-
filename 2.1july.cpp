18593a625929b4b208fb29b2c31e10f041d49fcc
3MEEHOCETC <tareycoylor@gmail.com>
2026-07-02 11:58:59 +0500

    2.1july

---
+++ 2.1july.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int getValue()
+{
+    std::cerr << "getValue() called\n";
+    return 4;
+}
+int main()
+{
+    // clang-format off
+    std::cerr << "main() called\n";
+    // clang-format on
+    std::cout << getValue << '\n';
+
+    return 0;
+}
