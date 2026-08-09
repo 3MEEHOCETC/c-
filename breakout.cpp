76e70f07488d4c39decde2ebe966c9187601c459
3MEEHOCETC <tareycoylor@gmail.com>
2026-08-09 20:41:42 +0500

    9th august

--- breakout.cpp
+++ breakout.cpp
@@ -92,6 +92,7 @@ while (!WindowShouldClose()) { // while - повторяй тело пока у�
         // крайний рывок
         //сдал на права, круто
         //dt делает скорость честной на лбьом железе
+        //я не купил мотоджинсы - без них защита колен только колени спасет
         if (paddle.rect.x < 0) paddle.rect.x = 0;
         if (paddle.rect.x + paddle.rect.width > screenWidth)
         paddle.rect.x = screenWidth - paddle.rect.width;
