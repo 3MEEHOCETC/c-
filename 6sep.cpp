f5884bb73c89593e6219b44e74b86649e6c7def7
3MEEHOCETC <tareycoylor@gmail.com>
2026-09-06 22:12:05 +0500

    6th sep

--- 6sep.cpp
+++ 6sep.cpp
@@ -3,14 +3,15 @@
 int main()
 {
     std::cout << "Enter an integer: ";
-    int x {};
+    int x{};
     std::cin >> x;
 
-    bool zero { (x == 0)};
-    if (zero)
+    if (x > 0)
+    std::cout << "The value is positive\n";
+    else if (x < 0)
+    std::cout << "The value is negative\n";
+    else
     std::cout << "The value is zero\n";
-    if (!zero)
-    std::cout << "The value is non-zero\n";
 
     return 0;
 }
